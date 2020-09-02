09ce8c5f09fa54ecddf4cabd839c1cc0f3c18c87
savanth <chinthireddysavanthreddy@gmail.com>
2020-09-02 16:27:13 +0530

    Add validations to Category model with the aid of unit tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
- class Category < ApplicationRecord
-
+class Category < ApplicationRecord
+	validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 26 }
 end
