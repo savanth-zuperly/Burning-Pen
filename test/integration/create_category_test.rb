636173534b82b54c1d998d35aa948917ef02d020
savanth <chinthireddysavanthreddy@gmail.com>
2020-09-03 10:56:25 +0530

    Add integration test to verify error messages on creating invalid category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
   test "get new category form and create category" do
   	get "/categories/new/"
   	assert :success
@@ -11,4 +12,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
   	follow_redirect!
   	assert_match "Travel",response.body 
   end
+
+  test "get new category form and reject an invalid category" do
+  	get "/categories/new/"
+  	assert :success
+  	assert_no_difference 'Category.count' do
+  		post "/categories", params: { category: {name: " "}}
+  	end
+  	assert_match   "errors",response.body
+  	assert_select 'div.alert'
+  	assert_select 'h4.alert-heading'
+  end
+
 end
